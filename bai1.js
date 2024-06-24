e426475a0abfaa354e45b8b759e09d482af81008
falconve <falconve@gmail.com>
2024-06-24 14:12:31 +0700

    Bai 1 / Buoi 1

---
+++ bai1.js
@@ -0,0 +1,16 @@
+// Để in dữ liệu
+// Dùng hàm console.log(); (Cách sử dụng nhiều nhất)
+console.log("Tuan anh");
+
+// Để in dữ liệu theo dạng pop-up
+// Dùng hàm alert();
+//alert("SD1801");
+
+// Để in dữ liệu lên html
+// Dùng hàm document.write()
+//document.write("Nhà f");
+
+// Input nhập vào
+// Dùng hàm prompt
+let input = prompt("Nhập điiiiiiii.......");
+document.write(input);
